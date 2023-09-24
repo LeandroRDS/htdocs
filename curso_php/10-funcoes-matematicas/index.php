1aec8b7604c64e789b1f49b6019b878132421c1e
leandrords <leandrords23012002@gmail.com>
2023-09-24 00:26:06 -0300

    dia 24 09 2023 00:25

--- curso_php/10-funcoes-matematicas/index.php
+++ curso_php/10-funcoes-matematicas/index.php
@@ -7,8 +7,47 @@
 </head>
 <body>
     <?php 
+        //entrga o valor real 
         $r = abs(500) ;
         print $r;
+        print "<br>";
+        //conversor de base
+        $bc = base_convert(254, 10, 8);
+        print $bc;
+        print "<br>";
+        $bc = base_convert(254, 10, 16);
+        print $bc;
+        print "<br>";
+        $bc = base_convert(254, 10, 2);
+        print $bc;
+        print "<br>";
+        $bc =base_convert(1011, 2, 10);
+        print $bc;
+        print "<br>";
+        //aredondamento 
+        //ciel() arredonda pra cima
+        //floor() arredonda pra baixo 
+        // round() arredondamento aritimetico 
+        //hypot() calcula a hipotenusa
+        $r = intdiv(5, 2);
+        print $r;
+        print "<br>";
+        print min(5, 2);
+        print "<br>";
+        print max(1,2,3,4,5,6,7,8,);
+        print pi();
+        print "<br>";
+        print M_PI;
+        print "<br>";
+        print pow(10,2);
+        print "<br>";
+        //raiz
+        print sqrt(81);
+        print "<br>";
+        print $r = 81 ** (1/2);
+        
+
+         
     ?>
 </body>
 </html>

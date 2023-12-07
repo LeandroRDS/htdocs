944b17a4ef9091c3e71c66d6650855c6fb20fd6e
leandrords <leandrords23012002@gmail.com>
2023-12-06 22:12:30 -0300

    fg

--- curso_dc/criacao_de_classes/classes.php
+++ curso_dc/criacao_de_classes/classes.php
@@ -1,19 +1,51 @@
 <?php 
     class DankiCode
     {
+
+
+        public $nomeStatic = '  leandro';
+
         function __construct($nome)
         {
             print $nome;
         }
 
-        public $nome = 'joão';
+        public $nome = '  joão';
         public function printNome(){
-            return 'Leandro';
+            return '  Leandro';
+        }
+
+        private function printNomeprivate(){
+            print '  Leandro';
+        }
+
+        public function printNomePublic(){
+            $this->printNomeprivate();
+        }
+
+        public static  function printNomeStatic(){
+            print'  laendroStaic';
+
         }
     }
 
-    $dankiCode = new dankiCode('leandro');
+    $dankiCode = new dankiCode('   leandro');
     print $dankiCode->printNome();
 
     print $dankiCode->nome;
+    $dankiCode->printNomePublic();
+
+    DankiCode::printNomeStatic();
+
+    $dankiCodestatic1 = new DankiCode('   leandrosStatic');
+
+    $dankiCodestatic1->nomeStatic = '   NomeAlterado';
+     
+    print $dankiCodestatic1->nomeStatic;
+
+    $dankiCodestatic2 = new DankiCode('  LeandroStatic2');
+
+    print $dankiCodestatic2->nomeStatic;
+
+
 ?>
